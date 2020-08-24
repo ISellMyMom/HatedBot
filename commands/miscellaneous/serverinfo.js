3f3f38002f783da76ca5fd38c9fd67f1b951d08a
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2020-08-25 00:39:07 +0300

    Update serverinfo.js

--- commands/miscellaneous/serverinfo.js
+++ commands/miscellaneous/serverinfo.js
@@ -1,26 +1,26 @@
-const { RichEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 const { cyan } = require("../../data/colours.json");
 
 module.exports = {
     config: {
         name: "serverinfo",
-        description: "Pulls the serverinfo of the guild!",
+        description: "Afişează informaţii despre server",
         usage: ``,
         category: "miscellaneous",
         accessableby: "Members",
         aliases: ["si", "serverdesc"]
     },
     run: async (bot, message, args) => {
-    let sEmbed = new RichEmbed()
+    let sEmbed = new MessageEmbed()
         .setColor(cyan)
         .setTitle("Server Info")
         .setThumbnail(message.guild.iconURL)
         .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
-        .addField("**Guild Name:**", `${message.guild.name}`, true)
-        .addField("**Guild Owner:**", `${message.guild.owner}`, true)
-        .addField("**Member Count:**", `${message.guild.memberCount}`, true)
-        .addField("**Role Count:**", `${message.guild.roles.size}`, true)
-        .setFooter(`TestBot | Footer`, bot.user.displayAvatarURL);
+        .addField("**Nume:**", `${message.guild.name}`, true)
+        .addField("**Fondator:**", `${message.guild.owner}`, true)
+        .addField("**Membrii:**", `${message.guild.memberCount}`, true)
+        .addField("**Roluri:**", `${message.guild.roles.size}`, true)
+        .setFooter(`HatedBot | Footer`, bot.user.displayAvatarURL);
     message.channel.send(sEmbed);
     }
 }
