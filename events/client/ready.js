61168c4f5aa2068202aad429566e3a346a378258
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2020-10-07 11:30:07 +0300

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 const { prefix } = require("../../data/config.json");
-
+const roleClaim = require("../../handlers/role-claim.js")
 module.exports = async bot => {
      console.log(`${bot.user.username} is online`)
      console.log(`Bot invite: https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=0`)
@@ -8,16 +8,16 @@ module.exports = async bot => {
         "Dorian Popa",
         `${prefix}help`,
         `BBC`,
-        `Netflix and chill with your GF`,
+        `Netflix with your GF`,
         `your pussy`,
         `Map 1`,
         `porn`
     ]
-    
+    bot.user.setStatus('dnd');
     setInterval(function() {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
         bot.user.setActivity(status, {type: "WATCHING"});
-
     }, 5000)
+    roleClaim(bot);
 
 }
