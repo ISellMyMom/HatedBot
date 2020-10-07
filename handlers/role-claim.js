a7c88e2a19357cabefe0fc1500891a31e2b9808d
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2020-10-07 11:29:12 +0300

    Add files via upload

---
+++ handlers/role-claim.js
@@ -0,0 +1,71 @@
+const firstMessage = require('./first-message')
+
+module.exports = (client) => {
+  const channelId = '763294071032643645'
+
+  const getEmoji = (emojiName) =>
+    client.emojis.cache.find((emoji) => emoji.name === emojiName)
+
+  const emojis = {
+    csgo: 'Counter Strike Global Offensive',
+    valorant: 'Valorant',
+    cod: 'Call Of Duty',
+    GTAV: 'Grand Theft Auto V',
+    rocketleague: "Rocket League",
+    samp: 'San Andreas: Multiplayer',
+    lol: 'League Of Legends',
+    dota: 'Dota 2',
+    amongus: 'Among Us',
+    fallguys: 'Fall Guys',
+    metin2: "Metin 2",
+  }
+
+  const reactions = []
+
+  let emojiText = '**Reactionează pentru a primi un rol:**\n\n'
+  for (const key in emojis) {
+    const emoji = getEmoji(key)
+    reactions.push(emoji)
+
+    const role = emojis[key]
+    emojiText += `${emoji} \`${role}\`\n`
+  }
+
+  firstMessage(client, channelId, emojiText, reactions)
+
+  const handleReaction = (reaction, user, add) => {
+    if (user.id === '747125728826359908') {
+      return
+    }
+
+    const emoji = reaction._emoji.name
+
+    const { guild } = reaction.message
+
+    const roleName = emojis[emoji]
+    if (!roleName) {
+      return
+    }
+
+    const role = guild.roles.cache.find((role) => role.name === roleName)
+    const member = guild.members.cache.find((member) => member.id === user.id)
+
+    if (add) {
+      member.roles.add(role)
+    } else {
+      member.roles.remove(role)
+    }
+  }
+
+  client.on('messageReactionAdd', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, true)
+    }
+  })
+
+  client.on('messageReactionRemove', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, false)
+    }
+  })
+}
