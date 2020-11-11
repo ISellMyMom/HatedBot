6d926eaa1b775a094d750985b2d7f51bcbad36c9
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2020-11-11 03:09:39 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 const { prefix } = require("../../data/config.json");
-const roleClaim = require("../../handlers/role-claim.js")
+
 module.exports = async bot => {
      console.log(`${bot.user.username} is online`)
      console.log(`Bot invite: https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=0`)
@@ -18,6 +18,5 @@ module.exports = async bot => {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
         bot.user.setActivity(status, {type: "WATCHING"});
     }, 5000)
-    roleClaim(bot);
 
 }
